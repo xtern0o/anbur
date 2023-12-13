cc09d89df8bd90fdcb5242e819fe278af98ce35d
xtern0o <dos30028gmail.com>
2023-12-13 22:11:46 +0300

    added: better frontend, coorect forms

--- source/main_window.py
+++ source/main_window.py
@@ -12,11 +12,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         QFontDatabase.addApplicationFont('fonts/Everson Mono.ttf')
         self.font = QFont('Everson Mono')
-        self.font.setPointSize(30)
-        self.matrix = [['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
+        self.font.setPointSize(24)
+        self.matrix = [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
+                       ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
                        ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'],
                        ['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю']]
-
         self.generate_keyboard()
 
     def generate_keyboard(self):
@@ -26,11 +26,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 btn = QPushButton(self)
                 btn.setFont(self.font)
                 btn.size()
-                btn.setText(anbur[word])
+                if word in anbur:
+                    btn.setText(anbur[word])
+                else:
+                    btn.setText(word)
+                btn.setMaximumSize(70, 70)
+                btn.setMinimumSize(70, 70)
+                btn.clicked.connect(self.clicked_on_btn)
                 size_policy = btn.sizePolicy()
-                size_policy.setHorizontalStretch(1)
-                size_policy.setVerticalStretch(1)
                 size_policy.setHeightForWidth(btn.sizePolicy().hasHeightForWidth())
                 btn.setSizePolicy(size_policy)
                 layout.addWidget(btn)
             self.keyboard_layout.addLayout(layout)
+
+    def clicked_on_btn(self):
+        btn = self.sender()
+        print(btn.text())
